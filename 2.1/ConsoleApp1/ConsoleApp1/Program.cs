0b287715d5c3e213ec8c9f1a22a61b72a790d321
BaranovaKN <126931773+BaranovaKN@users.noreply.github.com>
2023-06-15 20:05:40 +0700

    Add files via upload

---
+++ 2.1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ab";
+            string j = "aabbccd";
+            int counter = 0;
+            for (int i = 0; i < j.Length; i++)
+            {
+                if (j[i] == s[0] || j[i] == s[1])
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"s = {s}");
+            Console.WriteLine($"j = {j}");
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 2.1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+            bool counter = false;
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                for (int j = 1;j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    { 
+                        counter = true; 
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
